5ee1a71144c71f4d6caced26d8157563f3422e38
Abhishek <abhishekbhosale.dev@gmail.com>
2024-06-09 19:25:49 +0530

    feat: hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import Hero from "@/components/Hero";
+
 export default function Home() {
-  return <main className="bg-white pb-5 sm:pb-8 lg:pb-12"></main>;
+  return (
+    <main className="bg-white pb-5 sm:pb-8 lg:pb-12">
+      <Hero />
+    </main>
+  );
 }
