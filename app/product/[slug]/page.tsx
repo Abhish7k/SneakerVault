ad545e9fde3189dfd7d92341996cb2d2daa89726
Abhishek <abhishekbhosale.dev@gmail.com>
2024-06-10 14:31:16 +0530

    feat: product page
    created basic ui of single product page

---
+++ app/product/[slug]/page.tsx
@@ -0,0 +1,48 @@
+import ImageGallery from "@/components/ImageGallery";
+import { client } from "@/lib/sanity";
+
+interface fullProduct {
+  _id: string;
+  images: any;
+  price: number;
+  slug: string;
+  categoryName: string;
+  name: string;
+  description: string;
+  price_id: string;
+}
+
+async function getData(slug: string) {
+  const query = `*[_type == "product" && slug.current == "${slug}"][0] {
+          _id,
+            images,
+            price,
+            name,
+            description,
+            "slug": slug.current,
+            "categoryName": category->name,
+            price_id
+        }`;
+
+  const data = await client.fetch(query);
+
+  return data;
+}
+
+export default async function ProductPage({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const data: fullProduct = await getData(params.slug);
+
+  return (
+    <div className="bg-white">
+      <div className="mx-auto max-w-screen-xl px-4 md:px-8">
+        <div className="grid gap-8 md:grid-cols-2">
+          <ImageGallery images={data.images} />
+        </div>
+      </div>
+    </div>
+  );
+}
