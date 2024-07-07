e4fafe66eb17eb5166cb7bcb71a095430f8c9bb9
Abhishek <abhishekbhosale.dev@gmail.com>
2024-07-08 01:01:51 +0530

    feat: hero section
    improved appearance and change hero image

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,5 @@
 import { client, urlFor } from "@/lib/sanity";
 import Image from "next/image";
-import Link from "next/link";
 
 async function getData() {
   const query = "*[_type == 'heroImage'][0]";
@@ -14,38 +13,15 @@ export default async function Hero() {
   const data = await getData();
 
   return (
-    <section className="mx-auto max-w-2xl px-4 sm:pb-6 lg:max-w-7xl lg:px-8">
-      <div className="mb-8 flex flex-wrap justify-between md:mb-16">
-        <div className="mb-6 flex w-full flex-col justify-center sm:mb-12 lg:mb-0 lg:w-1/3 lg:pb-24 lg:pt-48">
-          <h1 className="mb-4 text-4xl font-bold text-black sm:text-5xl md:mb-8 md:text-6xl">
-            Step Up Your Game with Every Step
-          </h1>
-          <p className="max-w-md leading-relaxed text-gray-500 xl:text-lg">
-            Explore Our Exclusive Sneaker Collection!
-          </p>
-        </div>
-        <div className="mb-12 flex w-full md:mb-16 lg:w-2/3">
-          <div className="relative left-12 top-12 z-10 -ml-12 overflow-hidden rounded-lg bg-gray-100 shadow-lg md:left-16 md:top-16 lg:ml-0">
-            <Image
-              src={urlFor(data.image1).url()}
-              alt="Great Photo"
-              className="h-full w-full object-cover object-center"
-              priority
-              width={500}
-              height={500}
-            />
-          </div>
-
-          <div className="overflow-hidden rounded-lg bg-gray-100 shadow-lg">
-            <Image
-              src={urlFor(data.image2).url()}
-              alt="Great Photo"
-              className="h-full w-full object-cover object-center"
-              width={500}
-              height={500}
-              priority
-            />
-          </div>
+    <section className="mx-auto max-w-2xl mt-20 px-4 lg:max-w-7xl lg:px-8">
+      <div className="">
+        <div className="relative h-[500px] w-full">
+          <Image
+            src={urlFor(data.image1).url()}
+            alt="hero"
+            fill
+            className="h-full w-full object-cover rounded-xl"
+          />
         </div>
       </div>
     </section>
